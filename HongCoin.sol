d5dafcf725da5f0c142090d89d0429d099b3aced
hongcoiner <admin@hongcoin.org>
2016-07-10 19:21:07 +0800

    little more interfaces to map the idea from whitepaper

--- HongCoin.sol
+++ HongCoin.sol
@@ -22,8 +22,28 @@ along with the DO.  If not, see <http://www.gnu.org/licenses/>.
 
 contract TokenInterface {
     // define the coin itself
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+    // we suppose to be fully transparent for HongCoin's issuing
+    uint256 public totalSupply;
+
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
+    function balanceOf(address _owner) constant returns (uint256 balance);
+
+    /// @notice Send `_amount` tokens to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _amount The amount of tokens to be transferred
+    /// @return Whether the transfer was successful or not
+    function transfer(address _to, uint256 _amount) returns (bool success);
+
 }
 
 contract DOInterface {
     // define the governance of this organization and critical functions
+    function commmence() returns (bool);
+    function harvest() returns (bool);
+    function freezeFund() returns (bool);
+    function annualManagementFee() returns (bool);
+    function investProject() returns (bool);
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+Hong Coin DO contracts
+======================
+The contracts are based on the collaborative works from community, to create and manage a blockchain-based fund across borders. Some legal and finance related details will be bonded not only in these contracts, but also bounded to ground jurisdiction linking to the management firm.
