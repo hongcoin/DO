1380ed7a5ae399fec9583889a38343264224907c
hongcoiner <admin@hongcoin.org>
2016-07-11 19:18:24 +0800

    clean the comments to make it readable

--- HongCoin.sol
+++ HongCoin.sol
@@ -21,20 +21,11 @@ along with the DO.  If not, see <http://www.gnu.org/licenses/>.
 
 
 contract TokenInterface {
-    // define the coin itself
     mapping (address => uint256) balances;
     mapping (address => mapping (address => uint256)) allowed;
-    // we suppose to be fully transparent for HongCoin's issuing
     uint256 public totalSupply;
 
-    /// @param _owner The address from which the balance will be retrieved
-    /// @return The balance
     function balanceOf(address _owner) constant returns (uint256 balance);
-
-    /// @notice Send `_amount` tokens to `_to` from `msg.sender`
-    /// @param _to The address of the recipient
-    /// @param _amount The amount of tokens to be transferred
-    /// @return Whether the transfer was successful or not
     function transfer(address _to, uint256 _amount) returns (bool success);
 
 }
@@ -47,49 +38,15 @@ contract TokenInterface {
 
 contract TokenCreationInterface {
 
-    // End of token creation, in Unix time
     uint public closingTime;
-    // Minimum fueling goal of the token creation, denominated in tokens to
-    // be created
     uint public minTokensToCreate;
-    // True if the DAO reached its minimum fueling goal, false otherwise
     bool public isFueled;
-    // For DAO splits - if privateCreation is 0, then it is a public token
-    // creation, otherwise only the address stored in privateCreation is
-    // allowed to create tokens
     address public privateCreation;
-    // hold extra ether which has been sent after the DAO token
-    // creation rate has increased
     ManagedAccount public extraBalance;
-    // tracks the amount of wei given from each contributor (used for refund)
     mapping (address => uint256) weiGiven;
 
-    /// @dev Constructor setting the minimum fueling goal and the
-    /// end of the Token Creation
-    /// @param _minTokensToCreate Minimum fueling goal in number of
-    ///        Tokens to be created
-    /// @param _closingTime Date (in Unix time) of the end of the Token Creation
-    /// @param _privateCreation Zero means that the creation is public.  A
-    /// non-zero address represents the only address that can create Tokens
-    /// (the address can also create Tokens on behalf of other accounts)
-    // This is the constructor: it can not be overloaded so it is commented out
-    //  function TokenCreation(
-        //  uint _minTokensTocreate,
-        //  uint _closingTime,
-        //  address _privateCreation
-    //  );
-
-    /// @notice Create Token with `_tokenHolder` as the initial owner of the Token
-    /// @param _tokenHolder The address of the Tokens's recipient
-    /// @return Whether the token creation was successful
     function createTokenProxy(address _tokenHolder) returns (bool success);
-
-    /// @notice Refund `msg.sender` in the case the Token Creation did
-    /// not reach its minimum fueling goal
     function refund();
-
-    /// @return The divisor used to calculate the token creation rate during
-    /// the creation phase
     function divisor() constant returns (uint divisor);
 
     event FuelingToDate(uint value);
