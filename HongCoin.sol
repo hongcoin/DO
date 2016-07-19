0a25719e62dfb5f36f2e820f84f2bb32917bf093
William Lau <contact@cwlau.com>
2016-07-19 18:50:40 +0800

    Add some interfaces
    Added Token, TokenCreation, HongCoinInterface code.

--- HongCoin.sol
+++ HongCoin.sol
@@ -28,40 +28,32 @@ contract TokenInterface {
     function balanceOf(address _owner) constant returns (uint256 balance);
     function transfer(address _to, uint256 _amount) returns (bool success);
 
+    event Transfer(address indexed _from, address indexed _to, uint256 _amount);
 }
 
-/*
- * Token Creation contract, similar to other organization,for issuing tokens and initialize
- * its ether fund.
-*/
 
+contract Token is TokenInterface {
+    modifier noEther() {if (msg.value > 0) throw; _}
 
-contract TokenCreationInterface {
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+    }
 
-    uint public closingTime;
-    uint public minTokensToCreate;
-    bool public isFueled;
-    address public privateCreation;
-    ManagedAccount public extraBalance;
-    mapping (address => uint256) weiGiven;
-
-    function createTokenProxy(address _tokenHolder) returns (bool success);
-    function refund();
-    function divisor() constant returns (uint divisor);
-
-    event FuelingToDate(uint value);
-    event CreatedToken(address indexed to, uint amount);
-    event Refund(address indexed to, uint value);
+    function transfer(address _to, uint256 _amount) noEther returns (bool success) {
+        if (balances[msg.sender] >= _amount && _amount > 0) {
+            balances[msg.sender] -= _amount;
+            balances[_to] += _amount;
+            Transfer(msg.sender, _to, _amount);
+            return true;
+        } else {
+           return false;
+        }
+    }
 }
 
-contract GovernanceInterface {
-    // define the governance of this organization and critical functions
-    function kickoff(uint _fiscal) returns (bool);
-    function harvest() returns (bool);
-    function freezeFund() returns (bool);
-    function issueManagementFee() returns (bool);
-    function investProject() returns (bool);
-}
+
+
+
 
 
 contract ManagedAccountInterface {
@@ -97,3 +89,252 @@ contract ManagedAccount is ManagedAccountInterface{
         }
     }
 }
+
+
+
+
+
+/*
+ * Token Creation contract, similar to other organization,for issuing tokens and initialize
+ * its ether fund.
+*/
+
+
+contract TokenCreationInterface {
+
+    uint public closingTime;
+    uint public minTokensToCreate;
+    bool public isFueled;
+    address public privateCreation;
+    ManagedAccount public extraBalance;
+    mapping (address => uint256) weiGiven;
+
+    function createTokenProxy(address _tokenHolder) returns (bool success);
+    function refund();
+    function divisor() constant returns (uint divisor);
+
+    event FuelingToDate(uint value);
+    event CreatedToken(address indexed to, uint amount);
+    event Refund(address indexed to, uint value);
+}
+
+
+contract TokenCreation is TokenCreationInterface, Token {
+    function TokenCreation(
+        uint _minTokensToCreate,
+        uint _closingTime,
+        address _privateCreation) {
+
+        closingTime = _closingTime;
+        minTokensToCreate = _minTokensToCreate;
+        privateCreation = _privateCreation;
+        extraBalance = new ManagedAccount(address(this), true);
+    }
+
+    function createTokenProxy(address _tokenHolder) returns (bool success) {
+        if (now < closingTime && msg.value > 0
+            && (privateCreation == 0 || privateCreation == msg.sender)) {
+
+            uint token = (msg.value * 20) / divisor();
+            extraBalance.call.value(msg.value - token)();
+            balances[_tokenHolder] += token;
+            totalSupply += token;
+            weiGiven[_tokenHolder] += msg.value;
+            CreatedToken(_tokenHolder, token);
+            if (totalSupply >= minTokensToCreate && !isFueled) {
+                isFueled = true;
+                FuelingToDate(totalSupply);
+            }
+            return true;
+        }
+        throw;
+    }
+
+    function refund() noEther {
+        if (now > closingTime && !isFueled) {
+            // Get extraBalance - will only succeed when called for the first time
+            if (extraBalance.balance >= extraBalance.accumulatedInput())
+                extraBalance.payOut(address(this), extraBalance.accumulatedInput());
+
+            // Execute refund
+            if (msg.sender.call.value(weiGiven[msg.sender])()) {
+                Refund(msg.sender, weiGiven[msg.sender]);
+                totalSupply -= balances[msg.sender];
+                balances[msg.sender] = 0;
+                weiGiven[msg.sender] = 0;
+            }
+        }
+    }
+
+    function divisor() constant returns (uint divisor) {
+        // ------------------------------------------------------------
+        // TODO
+        // This depends on the final Coin Creation Scheme of HongCoin
+        // ------------------------------------------------------------
+
+        // The number of (base unit) tokens per wei is calculated
+        // as `msg.value` * 20 / `divisor`
+        // The fueling period starts with a 1:1 ratio
+        if (closingTime - 2 weeks > now) {
+            return 20;
+        // Followed by 10 days with a daily creation rate increase of 5%
+        } else if (closingTime - 4 days > now) {
+            return (20 + (now - (closingTime - 2 weeks)) / (1 days));
+        // The last 4 days there is a constant creation rate ratio of 1:1.5
+        } else {
+            return 30;
+        }
+    }
+}
+
+
+
+
+
+contract HongCoinInterface {
+    uint constant creationGracePeriod = 40 days;
+    uint constant minProposalDebatePeriod = 2 weeks;
+
+    // TODO: Confirm - do we need split?
+    // uint constant minSplitDebatePeriod = 1 weeks;
+    // uint constant splitExecutionPeriod = 27 days;
+
+    // TODO: Confirm the values below
+    uint constant quorumHalvingPeriod = 25 weeks;
+    uint constant executeProposalPeriod = 10 days;
+    // Denotes the maximum proposal deposit that can be given. It is given as
+    // a fraction of total Ether spent plus balance of the HongCoin
+    uint constant maxDepositDivisor = 100;
+
+    // Proposals to spend the HongCoin's ether or to choose a new Curator
+    Proposal[] public proposals;
+    // The quorum needed for each proposal is partially calculated by
+    // totalSupply / minQuorumDivisor
+    uint public minQuorumDivisor;
+    uint public lastTimeMinQuorumMet;
+
+    address public curator;
+    mapping (address => bool) public allowedRecipients;
+
+    mapping (address => uint) public rewardToken;
+    uint public totalRewardToken;
+
+    ManagedAccount public rewardAccount;
+    ManagedAccount public HongCoinRewardAccount;
+
+    mapping (address => uint) public HongCoinPaidOut;
+    mapping (address => uint) public paidOut;
+    mapping (address => uint) public blocked;
+
+    uint public proposalDeposit;
+    uint sumOfProposalDeposits;
+
+    // HongCoin_Creator public hongcoinCreator;
+
+    struct Proposal {
+        address recipient;
+        // The amount to transfer to `recipient` if the proposal is accepted.
+        uint amount;
+        // A plain text description of the proposal
+        string description;
+        // A unix timestamp, denoting the end of the voting period
+        uint votingDeadline;
+        // True if the proposal's votes have yet to be counted, otherwise False
+        bool open;
+        // True if quorum has been reached, the votes have been counted, and
+        // the majority said yes
+        bool proposalPassed;
+        // A hash to check validity of a proposal
+        bytes32 proposalHash;
+        // Deposit in wei the creator added when submitting their proposal. It
+        // is taken from the msg.value of a newProposal call.
+        uint proposalDeposit;
+        // True if this proposal is to assign a new Curator
+        bool newCurator;
+
+        // Number of Tokens in favor of the proposal
+        uint yea;
+        // Number of Tokens opposed to the proposal
+        uint nay;
+        // Simple mapping to check if a shareholder has voted for it
+        mapping (address => bool) votedYes;
+        // Simple mapping to check if a shareholder has voted against it
+        mapping (address => bool) votedNo;
+        // Address of the shareholder who created the proposal
+        address creator;
+    }
+
+    // Used to restrict access to certain functions to only DAO Token Holders
+    modifier onlyTokenholders {}
+
+    function () returns (bool success);
+    function receiveEther() returns(bool);
+
+    function newProposal(
+        address _recipient,
+        uint _amount,
+        string _description,
+        bytes _transactionData,
+        uint _debatingPeriod,
+        bool _newCurator
+    ) onlyTokenholders returns (uint _proposalID);
+
+    function checkProposalCode(
+        uint _proposalID,
+        address _recipient,
+        uint _amount,
+        bytes _transactionData
+    ) constant returns (bool _codeChecksOut);
+
+    function vote(
+        uint _proposalID,
+        bool _supportsProposal
+    ) onlyTokenholders returns (uint _voteID);
+
+    function executeProposal(
+        uint _proposalID,
+        bytes _transactionData
+    ) returns (bool _success);
+
+    function newContract(address _newContract);
+    function changeAllowedRecipients(address _recipient, bool _allowed) external returns (bool _success);
+    function changeProposalDeposit(uint _proposalDeposit) external;
+    function retrieveHongCoinReward(bool _toMembers) external returns (bool _success);
+    function getMyReward() returns(bool _success);
+    function withdrawRewardFor(address _account) internal returns (bool _success);
+    function transferWithoutReward(address _to, uint256 _amount) returns (bool success);
+    function transferFromWithoutReward(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) returns (bool success);
+    function halveMinQuorum() returns (bool _success);
+    function numberOfProposals() constant returns (uint _numberOfProposals);
+    function getNewHongCoinAddress(uint _proposalID) constant returns (address _newHongCoin);
+    function isBlocked(address _account) internal returns (bool);
+    function unblockMe() returns (bool);
+
+    event ProposalAdded(
+        uint indexed proposalID,
+        address recipient,
+        uint amount,
+        bool newCurator,
+        string description
+    );
+    event Voted(uint indexed proposalID, bool position, address indexed voter);
+    event ProposalTallied(uint indexed proposalID, bool result, uint quorum);
+    event NewCurator(address indexed _newCurator);
+    event AllowedRecipientChanged(address indexed _recipient, bool _allowed);
+}
+
+
+
+contract GovernanceInterface {
+    // define the governance of this organization and critical functions
+    function kickoff(uint _fiscal) returns (bool);
+    function harvest() returns (bool);
+    function freezeFund() returns (bool);
+    function issueManagementFee() returns (bool);
+    function investProject() returns (bool);
+}
+
